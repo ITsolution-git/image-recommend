aaeec4b91910fa461332c32d4ef1bb3288b75df2
Roy Smith <roy.smith0820@gmail.com>
2017-02-14 12:21:38 -0500

    deploy

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
-angular.module('app', ['ngRoute', 'appRoutes', 'MainCtrl', 'ImageCtrl','ImageService']);
+angular.module('app', ['ngRoute', 'appRoutes', 'MainCtrl', 'ImageCtrl','ImageService', 'angularLazyImg'])
+.config(['lazyImgConfigProvider', function(lazyImgConfigProvider){
+    lazyImgConfigProvider.setOptions({
+      offset: 100, // how early you want to load image (default = 100)
+      errorClass: 'error', // in case of loading image failure what class should be added (default = null)
+      successClass: 'success', // in case of loading image success what class should be added (default = null)
+      onError: function(image){}, // function fired on loading error
+      onSuccess: function(image){}, // function fired on loading success
+    });
+}]);	
